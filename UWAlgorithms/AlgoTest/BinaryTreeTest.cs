2a2a3a2920aaf686944d34110db77fde87b761fd
kcq218 <khangquach218@gmail.com>
2020-05-21 21:05:27 -0700

    BTS week 6 notes

---
+++ UWAlgorithms/AlgoTest/BinaryTreeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgoTest
+{
+    [TestClass]
+    public class BinaryTreeTest
+    {
+
+        internal class TreeNode
+        {
+            public int value;
+            public TreeNode leftNode;
+            public TreeNode rightNode;
+        }
+    }
+}
