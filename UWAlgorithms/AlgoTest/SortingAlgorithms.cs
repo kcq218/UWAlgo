3c25b3d135de22c45a3f1c48e2795324dd2b0b5c
kcq218 <khangquach218@gmail.com>
2020-10-16 19:31:15 -0700

    MergeSort

--- UWAlgorithms/AlgoTest/SortingAlgorithms.cs
+++ UWAlgorithms/AlgoTest/SortingAlgorithms.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AlgoTest
 {
     [TestClass]
-    class SortingAlgorithms
+    public class SortingAlgorithms
     {
         [TestMethod]
         public void testMergeSort()
@@ -25,20 +21,92 @@ namespace AlgoTest
             Assert.AreEqual(test[5], 11);
         }
 
-        private void mergsort(int[] test)
+        [TestMethod]
+        public void testQuickSort()
+        {
+            int[] test = new int[] { 0, -1, -3, 9, 11, 3 };
+
+            quickSort(test);
+
+            Assert.AreEqual(test[0], -3);
+            Assert.AreEqual(test[1], -1);
+            Assert.AreEqual(test[2], 0);
+            Assert.AreEqual(test[3], 3);
+            Assert.AreEqual(test[4], 9);
+            Assert.AreEqual(test[5], 11);
+        }
+
+        private void quickSort(int[] test)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void mergsort(int[] test)
         {
             doMergeSort(test, 0, test.Length - 1);
         }
 
         private void doMergeSort(int[] test, int v1, int v2)
         {
-            var middle = (v2 - v1) / 2; 
+            var middle = v1 + (v2 - v1) / 2; 
             
-            if(middle > v1)
+            if(v1 < v2)
             {
                 doMergeSort(test,v1, middle);
-                doMergeSort(test, middle, v2);
-                merge()
+                doMergeSort(test, middle+1, v2);
+                merge(test, v1, middle, v2);
+            }
+        }
+
+        private void merge(int[] test, int v1, int middle, int v2)
+        {
+            int[] copyarr = new int[middle - v1 + 1];
+            int[] copyarr2 = new int[v2 - middle];
+
+            for(int i = 0; i < middle - v1 + 1; i++)
+            {
+                copyarr[i] = test[i + v1];
+            }
+
+            for (int x = 0; x < v2 - middle; x++)
+            {
+                copyarr2[x] = test[middle + 1 + x];
+            }
+
+            var index0 = 0;
+            var index2 = 0;
+            var indexTest = v1;
+
+            while(index0 < copyarr.Length && index2 < copyarr2.Length && indexTest < test.Length)
+            {
+                if (copyarr[index0] < copyarr2[index2])
+                {
+                    test[indexTest] = copyarr[index0];
+                    index0++;
+                }
+                else
+                {
+                    test[indexTest] = copyarr2[index2];
+                    index2++;
+                }
+
+                indexTest++;
+            }
+
+            while(index0 < copyarr.Length)
+            {
+                test[indexTest] = copyarr[index0];
+
+                index0++;
+                indexTest++;
+            }
+
+            while (index2 < copyarr2.Length)
+            {
+                test[indexTest] = copyarr2[index2];
+
+                index2++;
+                indexTest++;
             }
         }
     }
