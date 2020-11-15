c60d714aac8ee0e97350943f4fe247f9d5ba867f
kcq218 <khangquach218@gmail.com>
2020-11-14 22:26:10 -0800

    Created Hashmap.

--- UWAlgorithms/UWAlgorithms/LinearProbing.cs
+++ UWAlgorithms/UWAlgorithms/LinearProbing.cs
@@ -1,231 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace UWAlgorithms
 {
-    //class LinearProbing
-    //{
-    //    private int currentSize, maxSize;
-    //    private String[] keys;
-    //    private String[] vals;
+    // attempt 2 at Hashmaps
 
-    //    public LinearProbing(int capacity)
-    //    {
+    class LinearProbing
+    {
+        //have buckets
+        public LinearProbing(int capacity)
+        {
 
-    //    }
+        }
+    }
 
-    //    public String get(String key)
-    //    {
-    //        int index = key.GetHashCode() % 10;
+    public class HashMap<K, V>
+    {
+        private int size;
+        private int capacity;
+        private List<HashBucket<K,V>> buckets;
+        public HashMap(int capacity)
+        {
+            this.capacity = capacity;
+            size = 0;
 
-    //        while (keys[index] != null)
-    //        {
-    //            if (keys[index] == key)
-    //            {
-    //                return vals[index];
-    //            }
+            buckets = new List<HashBucket<K,V>>();
 
-    //            index = (index + 1) % 10;
-    //        }
+            for (int i = 0; i < capacity; i++)
+            {
+                buckets.Add(null);
+            }
+        }
 
-    //        return null;
-    //    }
+        // so index = k.getHashCode % capacity
+        // if( hahsBucket(index)[K] = K
+        // update value
+        // while hashbucket[index] not null
+        // if key is the same update value
+        // or else index++ and rehash
+        // if hashbuckets[index] == null
+        // create new hashbucket with key and value
+        // take care of loadfactor.
 
-    //    public void insert(String key, String value)
-    //    {
-    //        int index = key.GetHashCode() % 10;
+        public void add(K key,V Value)
+        {
+            if (size / capacity >= .75)
+            {
+                var tempbuckets = buckets;
 
-    //        while (keys[index] != null && keys[index] != value)
-    //        {
-    //            index = (index + 1) % 10;
-    //        }
+                capacity = capacity * 2;
+                buckets = new List<HashBucket<K, V>>();
 
-    //        if (keys[index] == value)
-    //        {
-    //            return;
-    //        }
+                for (int i = 0; i < capacity; i++)
+                {
+                    buckets[i] = null;
+                }
 
-    //        keys[index] = value;
-    //    }
+                foreach (var item in tempbuckets)
+                {
+                    if(item != null)
+                    {
+                        AddHelper(item.key, item.value);
+                    }
+                }
+            }
+            else
+            {
+                AddHelper(key, Value);
+            }
+        }
 
-    //    public bool exist(String key)
-    //    {
-    //        return get(key) != null;
-    //    }
+        void AddHelper(K key, V Value)
+        {
+            var index = key.GetHashCode() % capacity;
 
-    //    public void remove(String key)
-    //    {
-    //        if (!exist(key))
-    //        {
-    //            return;
-    //        }
+            while (buckets[index] != null)
+            {
+                if (buckets[index].key.Equals(key))
+                {
+                    buckets[index].value = Value;
 
-    //        int index = key.GetHashCode() % 10;
+                    return;
+                }
+                else
+                {
+                    index++;
+                    index = key.GetHashCode() % capacity;
+                }
+            }
 
-    //        while (key != (keys[index]))
-    //        {
-    //            index = (index + 1) % 10;
-    //        }
+            buckets[index] = new HashBucket<K, V>(key, Value);
+            size++;
+        }
+        public HashBucket<K,V> Get(K Key)
+        {
+            foreach (var bucket in buckets)
+            {
+                if(bucket != null)
+                {
+                    if(bucket.key.Equals(Key))
+                    {
+                        return bucket;
+                    }
+                }    
+            }
 
-    //        keys[index] = null;
-    //        vals[index] = null;
+            return null;
+        }
 
-    //        for (index = (index + 1) % 10; keys[index] != null; index = (index + 1) % maxSize)
-    //        {
-    //            var tempKey = keys[index];
-    //            var tempValue = vals[index];
+        public bool Exist(K key)
+        {
+            return Get(key) != null;
+        }
 
-    //            keys[index] = null;
-    //            vals[index] = null;
+        public void Remove(K key)
+        {
+            for (int i = 0; i < capacity; i++)
+            {
+                if(buckets[i] != null && buckets[i].key.Equals(key))
+                {
+                    buckets[i] = null;
+                }
+            }
+        }
+    }
 
-    //            currentSize--;
-    //            insert(tempKey, tempValue);
-    //        }
+    public class HashBucket<K,V>
+    {
+        public K key;
+        public V value;
 
-    //        currentSize--;
-    //    }
-
-    //    class Map<K, V>
-    //    {
-    //        // bucketArray is used to store array of chains 
-    //        private ArrayList<HashNode<K, V>> bucketArray;
-
-    //        // Current capacity of array list 
-    //        private int numBuckets;
-
-    //        // Current size of array list 
-    //        private int size;
-
-    //        // Constructor (Initializes capacity, size and 
-    //        // empty chains. 
-    //        public Map()
-    //        {
-    //            bucketArray = new ArrayList<>();
-    //            numBuckets = 10;
-    //            size = 0;
-
-    //            // Create empty chains 
-    //            for (int i = 0; i < numBuckets; i++)
-    //                bucketArray.add(null);
-    //        }
-
-    //        public int size() { return size; }
-    //        public boolean isEmpty() { return size() == 0; }
-
-    //        // This implements hash function to find index 
-    //        // for a key 
-    //        private int getBucketIndex(K key)
-    //        {
-    //            int hashCode = key.hashCode();
-    //            int index = hashCode % numBuckets;
-    //            return index;
-    //        }
-
-    //        // Method to remove a given key 
-    //        public V remove(K key)
-    //        {
-    //            // Apply hash function to find index for given key 
-    //            int bucketIndex = getBucketIndex(key);
-
-    //            // Get head of chain 
-    //            HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-    //            // Search for key in its chain 
-    //            HashNode<K, V> prev = null;
-    //            while (head != null)
-    //            {
-    //                // If Key found 
-    //                if (head.key.equals(key))
-    //                    break;
-
-    //                // Else keep moving in chain 
-    //                prev = head;
-    //                head = head.next;
-    //            }
-
-    //            // If key was not there 
-    //            if (head == null)
-    //                return null;
-
-    //            // Reduce size 
-    //            size--;
-
-    //            // Remove key 
-    //            if (prev != null)
-    //                prev.next = head.next;
-    //            else
-    //                bucketArray.set(bucketIndex, head.next);
-
-    //            return head.value;
-    //        }
-
-    //        // Returns value for a key 
-    //        public V get(K key)
-    //        {
-    //            // Find head of chain for given key 
-    //            int bucketIndex = getBucketIndex(key);
-    //            HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-    //            // Search key in chain 
-    //            while (head != null)
-    //            {
-    //                if (head.key.equals(key))
-    //                    return head.value;
-    //                head = head.next;
-    //            }
-
-    //            // If key not found 
-    //            return null;
-    //        }
-
-    //        // Adds a key value pair to hash 
-    //        public void add(K key, V value)
-    //        {
-    //            // Find head of chain for given key 
-    //            int bucketIndex = getBucketIndex(key);
-    //            HashNode<K, V> head = bucketArray.get(bucketIndex);
-
-    //            // Check if key is already present 
-    //            while (head != null)
-    //            {
-    //                if (head.key.equals(key))
-    //                {
-    //                    head.value = value;
-    //                    return;
-    //                }
-    //                head = head.next;
-    //            }
-
-    //            // Insert key in chain 
-    //            size++;
-    //            head = bucketArray.get(bucketIndex);
-    //            HashNode<K, V> newNode = new HashNode<K, V>(key, value);
-    //            newNode.next = head;
-    //            bucketArray.set(bucketIndex, newNode);
-
-    //            // If load factor goes beyond threshold, then 
-    //            // double hash table size 
-    //            if ((1.0 * size) / numBuckets >= 0.7)
-    //            {
-    //                ArrayList<HashNode<K, V>> temp = bucketArray;
-    //                bucketArray = new ArrayList<>();
-    //                numBuckets = 2 * numBuckets;
-    //                size = 0;
-    //                for (int i = 0; i < numBuckets; i++)
-    //                    bucketArray.add(null);
-
-    //                for (HashNode<K, V> headNode : temp)
-    //                {
-    //                    while (headNode != null)
-    //                    {
-    //                        add(headNode.key, headNode.value);
-    //                        headNode = headNode.next;
-    //                    }
-    //                }
-    //            }
-    //        }
-
-    //    }
-    //}
+        public HashBucket(K Key, V Value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
 }
